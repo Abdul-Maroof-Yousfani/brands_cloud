d2719623a4814bce1edeb4d4d2839048935d3240
HashimAbbas555 <hashim.abbas@innovative-net.com>
2025-12-12 23:56:11 +0500

    bug fix

--- app/Http/Controllers/SalesReturnReportController.php
+++ app/Http/Controllers/SalesReturnReportController.php
@@ -28,9 +28,9 @@ class SalesReturnReportController extends Controller
                 ->when($so, function ($q) use ($so_id) {
                     $q->where("credit_note.so_id", "like", "%{$so_id}%");
                 })
-                ->when(isset($request->from) && isset($request->to), function($query) use ($request) {
-                    $query->whereBetween("credit_note_data.date", [$request->from, $request->to]);
-                })
+                // ->when(isset($request->from) && isset($request->to), function($query) use ($request) {
+                //     $query->whereBetween("credit_note_data.date", [$request->from, $request->to]);
+                // })
                 // ->whereBetween("credit_note_data.date", [$request->from, $request->to])
                 ->groupBy("subitem.product_barcode")
                 ->select(
