2856f1b3e19cbdcb3068e4e7d5efb698d8c9ba8c
HashimAbbas555 <hashim.abbas@innovative-net.com>
2025-12-12 21:26:35 +0500

    validation message

--- app/Http/Controllers/SalesAddDetailControler.php
+++ app/Http/Controllers/SalesAddDetailControler.php
@@ -628,7 +628,7 @@ class SalesAddDetailControler extends Controller
         ]);
 
         if ($validator->fails()) {
-            return redirect()->back()->with(["success" => false, "message" => $validator->errors()]);
+            return redirect()->back()->with(["error" => implode('<br>', $validator->errors()->all()), "status" => 404]);
             // return response()->json(['errors' => $validator->errors()], 422);
         }
 
