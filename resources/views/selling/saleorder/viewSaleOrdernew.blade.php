096ab04a5d9074456cf194a997f840ca5c722f4e
Saquib Saleem <125876033+SaquibSaleem-INPL@users.noreply.github.com>
2025-12-18 17:15:34 +0500

    Update viewSaleOrdernew.blade.php

--- resources/views/selling/saleorder/viewSaleOrdernew.blade.php
+++ resources/views/selling/saleorder/viewSaleOrdernew.blade.php
@@ -76,7 +76,7 @@ input.form-control.form-control2{margin:0!important;}
                                                     {{ CommonHelper::get_all_country_by_id($buyer_detail->country)->name ?? '-'}}<br>
                                                     <!-- {{$buyer_detail->phone_1}}<br> -->
                                                     N.T.N #:{{isset($buyer_detail->cnic_ntn) ? $buyer_detail->cnic_ntn : "-" }}<br>
-                                                    S.T.R.N #: {{isset($buyer_detail->strn) ? $buyer_detail->strn : "-"}}
+                                                    S.T.R.N #: {{isset($buyer_detail->strn) ? $buyer_detail->strn : "3277876156235"}}
                                                 </p>
                                               
                                             </div>
