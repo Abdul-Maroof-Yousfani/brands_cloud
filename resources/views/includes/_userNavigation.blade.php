dc39aad5e960ad4a2628e53ad5aa4a4c7468a358
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-02-09 13:03:46 +0500

    users navigation

--- resources/views/includes/_userNavigation.blade.php
+++ resources/views/includes/_userNavigation.blade.php
@@ -197,6 +197,27 @@ endif;
               </li>
              
            </ul>
+           @php
+                $pending_delivery_notes = \App\Helpers\CommonHelper::pendingDocuments("delivery_note", "status", 0);
+                $pending_sale_tax_invoices = \App\Helpers\CommonHelper::pendingDocuments("sales_tax_invoice", "si_status", 1, "status", 1, true);
+                $pending_sale_returns = \App\Helpers\CommonHelper::pendingDocuments("credit_note", "status", 0);
+                $pending_purchase_requests = \App\Helpers\CommonHelper::pendingDocuments("demand", "demand_status", 1, "status", 1);
+                $pending_purchase_quotations = \App\Helpers\CommonHelper::pendingDocuments("quotation", "quotation_status", 1, "status", 1);
+                $pending_purchase_orders = \App\Helpers\CommonHelper::pendingDocuments("purchase_request", "purchase_request_status", 1, "status", 1);
+                $pending_grns = \App\Helpers\CommonHelper::pendingDocuments("goods_receipt_note", "grn_status", 1, "status", 1);
+                $pending_purchase_invoices = \App\Helpers\CommonHelper::pendingDocuments("new_purchase_voucher", "pv_status", 1, "status", 1);
+                $pending_stock_transfers = \App\Helpers\CommonHelper::pendingDocuments("stock_transfer", "tr_status", 1, "status", 1);
+                $total_pending = 
+                    $pending_delivery_notes +
+                    $pending_sale_tax_invoices +
+                    $pending_sale_returns +
+                    $pending_purchase_requests +
+                    $pending_purchase_quotations +
+                    $pending_purchase_orders +
+                    $pending_grns +
+                    $pending_purchase_invoices +
+                    $pending_stock_transfers;
+            @endphp
            <ul class="profile-admin d-flex">
              
               
@@ -204,7 +225,7 @@ endif;
                 <a class="nav-link bella" href="javascript:void(0)" data-bs-toggle="dropdown" aria-expanded="false">
                     <i class="fa fa-bell" aria-hidden="true"></i>
                     <span
-                        class="badge rounded-pill bg-danger badge-up notification-count">{{ App\Helpers\CommonHelper::countOfUnreadMessages() }}</span>
+                        class="badge rounded-pill bg-danger badge-up notification-count">{{ $total_pending }}</span>
                 </a>
 
                 <ul class="dropdown-menu dropdown-menu-media dropdown-menu-end">
@@ -212,28 +233,132 @@ endif;
                         <div class="dropdown-header d-flex">
                             <h4 class="notification-title mb-0 me-auto">Notifications</h4>
                             <div class="badge rounded-pill badge-light-primary"><span
-                                    class="notification-count">{{ App\Helpers\CommonHelper::countOfUnreadMessages() }}</span>
+                                    class="notification-count">{{ $total_pending }}</span>
                                 New</div>
                         </div>
                     </li>
 
                     <li class="scrollable-container media-list">
 
-                        @foreach (App\Helpers\CommonHelper::getUnreadNotifications() as $notification)
-                            <a class="d-flex" href="javascript:void(0)">
+                        @if($pending_delivery_notes > 0)
+                            <a class="d-flex" href="/sales/viewDeliveryNoteList?m={{ request()->m }}&parentCode={{ request()->parentCode }}">
                                 <div class="list-item d-flex align-items-start">
                                     <div class="list-item-body flex-grow-1">
                                         <p class="media-heading">
-                                            <span class="fw-bolder">{{ $notification->heading }}</span>
+                                            <span class="fw-bolder">Delivery Note</span>
                                         </p>
-                                        <small class="notification-text">{{ $notification->message }}</small>
+                                        <small class="notification-text">{{ $pending_delivery_notes }} Delivery Notes are pending</small>
                                         <br>
-                                        <small class="text-muted"
-                                            style="font-size: 0.75rem;">{{ \Carbon\Carbon::parse($notification->created_at)->diffForHumans() }}</small>
                                     </div>
                                 </div>
                             </a>
-                        @endforeach
+                        @endif
+                        @if($pending_sale_tax_invoices > 0)
+                            <a class="d-flex" href="/sales/viewSalesTaxInvoiceList?m={{ request()->m }}&parentCode={{ request()->parentCode }}">
+                                <div class="list-item d-flex align-items-start">
+                                    <div class="list-item-body flex-grow-1">
+                                        <p class="media-heading">
+                                            <span class="fw-bolder">Sales Tax Invoice</span>
+                                        </p>
+                                        <small class="notification-text">{{ $pending_sale_tax_invoices }} Sales Tax Invoice are pending</small>
+                                        <br>
+                                    </div>
+                                </div>
+                            </a>
+                        @endif
+                        @if($pending_sale_returns > 0)
+                             <a class="d-flex" href="/sales/viewCustomerCreditNoteList?m={{ request()->m }}&parentCode={{ request()->parentCode }}">
+                                <div class="list-item d-flex align-items-start">
+                                    <div class="list-item-body flex-grow-1">
+                                        <p class="media-heading">
+                                            <span class="fw-bolder">Sales Return</span>
+                                        </p>
+                                        <small class="notification-text">{{ $pending_sale_returns }} Sale Returns are pending</small>
+                                        <br>
+                                    </div>
+                                </div>
+                            </a>
+                        @endif
+                        @if($pending_purchase_requests > 0)
+                            <a class="d-flex" href="/purchase/viewDemandList?m={{ request()->m }}&parentCode={{ request()->parentCode }}">
+                                <div class="list-item d-flex align-items-start">
+                                    <div class="list-item-body flex-grow-1">
+                                        <p class="media-heading">
+                                            <span class="fw-bolder">Purchase Requests</span>
+                                        </p>
+                                        <small class="notification-text">{{ $pending_purchase_requests }} Purchase Requests are pending</small>
+                                        <br>
+                                    </div>
+                                </div>
+                            </a>
+                        @endif
+                        @if($pending_purchase_quotations > 0)
+                            <a class="d-flex" href="/quotation/quotation_list?m={{ request()->m }}&parentCode={{ request()->parentCode }}">
+                                <div class="list-item d-flex align-items-start">
+                                    <div class="list-item-body flex-grow-1">
+                                        <p class="media-heading">
+                                            <span class="fw-bolder">Purchase Quotations</span>
+                                        </p>
+                                        <small class="notification-text">{{ $pending_purchase_quotations }} Purchase Quotations are pending</small>
+                                        <br>
+                                    </div>
+                                </div>
+                            </a>
+                        @endif
+                        @if($pending_purchase_invoices > 0)
+                            <a class="d-flex" href="/store/viewPurchaseRequestList?m={{ request()->m }}&parentCode={{ request()->parentCode }}">
+                                <div class="list-item d-flex align-items-start">
+                                    <div class="list-item-body flex-grow-1">
+                                        <p class="media-heading">
+                                            <span class="fw-bolder">Purchase Orders</span>
+                                        </p>
+                                        <small class="notification-text">{{ $pending_purchase_orders }} Purchase Orders are pending</small>
+                                        <br>
+                                    </div>
+                                </div>
+                            </a>
+                        @endif
+                        @if($pending_grns > 0)
+                            <a class="d-flex" href="/purchase/viewGoodsReceiptNoteList?m={{ request()->m }}&parentCode={{ request()->parentCode }}">
+                                <div class="list-item d-flex align-items-start">
+                                    <div class="list-item-body flex-grow-1">
+                                        <p class="media-heading">
+                                            <span class="fw-bolder">Goods Receipt Note</span>
+                                        </p>
+                                        <small class="notification-text">{{ $pending_grns }} Goods Receipt Note are pending</small>
+                                        <br>
+                                    </div>
+                                </div>
+                            </a>
+                        @endif
+
+                        @if($pending_purchase_invoices > 0)
+                            <a class="d-flex" href="/purchase/viewPurchaseVoucherListThroughGrn?m={{ request()->m }}&parentCode={{ request()->parentCode }}">
+                                <div class="list-item d-flex align-items-start">
+                                    <div class="list-item-body flex-grow-1">
+                                        <p class="media-heading">
+                                            <span class="fw-bolder">Purchase Invoice</span>
+                                        </p>
+                                        <small class="notification-text">{{ $pending_purchase_invoices }} Purchase Invoices are pending</small>
+                                        <br>
+                                    </div>
+                                </div>
+                            </a>
+                        @endif
+
+                        @if($pending_stock_transfers > 0)
+                            <a class="d-flex" href="/store/stock_transfer_list?m={{ request()->m }}&parentCode={{ request()->parentCode }}">
+                                <div class="list-item d-flex align-items-start">
+                                    <div class="list-item-body flex-grow-1">
+                                        <p class="media-heading">
+                                            <span class="fw-bolder">Stock Transfer</span>
+                                        </p>
+                                        <small class="notification-text">{{ $pending_stock_transfers }} Stock Transfers are pending</small>
+                                        <br>
+                                    </div>
+                                </div>
+                            </a>
+                        @endif
 
 
                     <li class="dropdown-menu-footer">
